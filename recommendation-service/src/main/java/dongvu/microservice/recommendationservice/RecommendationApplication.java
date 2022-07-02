ce901281873fd09950d313e6521344da5450ab72
dongvu055 <dongvu055@gmail.com>
2022-07-03 00:19:34 +0700

    add

---
+++ recommendation-service/src/main/java/dongvu/microservice/recommendationservice/RecommendationApplication.java
@@ -0,0 +1,11 @@
+package dongvu.microservice.recommendationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationApplication.class,args);
+    }
+}
